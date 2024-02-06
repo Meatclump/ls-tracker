018118b45550718e9b556f4d269e87bfa16b204b
Meatclump <11631528+Meatclump@users.noreply.github.com>
2024-02-06 15:50:56 -0600

    Added font awesome icons

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,14 +1,16 @@
 'use client'
 
+import { faCloud, faDove, faFireFlameCurved, faFireFlameSimple, faFish, faHouse } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 const links = [
-	{ name: "Home", href: "/dashboard" },
-	{ name: "Dynamis", href: "/dashboard/dynamis" },
-	{ name: "Limbus", href: "/dashboard/limbus", disabled: true },
-	{ name: "Sea", href: "/dashboard/sea", disabled: true },
-	{ name: "Sky", href: "/dashboard/sky", disabled: true },
+	{ name: "Home", href: "/dashboard", icon: <FontAwesomeIcon icon={faHouse} /> },
+	{ name: "Dynamis", href: "/dashboard/dynamis", icon: <FontAwesomeIcon  icon={faFireFlameCurved}/> },
+	{ name: "Limbus", href: "/dashboard/limbus", icon: <FontAwesomeIcon icon={faFireFlameSimple} />, disabled: true },
+	{ name: "Sea", href: "/dashboard/sea", icon: <FontAwesomeIcon icon={faFish} />, disabled: true },
+	{ name: "Sky", href: "/dashboard/sky", icon: <FontAwesomeIcon icon={faDove} />, disabled: true },
 ]
 
 export default function NavLinks() {
@@ -24,9 +26,10 @@ export default function NavLinks() {
 								type="button"
 								key={link.name}
 								disabled={true}
-								className="md:flex-none h-[48px] w-full grow gap-2 font-medium bg-zinc-800/35 text-neutral-100/35 rounded-md p-3 text-sm text-start"
+								className="flex md:flex-none items-center h-[48px] grow gap-2 font-medium bg-zinc-800/35 text-neutral-100/35 rounded-md p-3 text-sm text-start"
 							>
-								{link.name}
+								{link.icon}
+								<p>{link.name}</p>
 							</button>
 						)
 					} else {
@@ -34,9 +37,10 @@ export default function NavLinks() {
 							<Link
 								key={link.name}
 								href={link.href}
-								className={`${pathname === link.href ? 'bg-amber-500 text-amber-950' : 'bg-zinc-800 text-neutral-300'} md:flex-none h-[48px] w-full grow gap-2 font-medium rounded-md p-3 text-sm hover:bg-amber-500 hover:text-amber-950`}
+								className={`${pathname === link.href ? 'bg-amber-500 text-amber-950' : 'bg-zinc-800 text-neutral-300'} flex items-center h-[48px] text-start gap-2 font-medium rounded-md p-3 text-sm hover:bg-amber-500 hover:text-amber-950`}
 							>
-								<p className="">{link.name}</p>
+								{link.icon}
+								<p>{link.name}</p>
 							</Link>
 						)
 					}

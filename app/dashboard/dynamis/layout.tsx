ca4e57f100a8582215c99437c7e2155fea845266
Henrik Persson <hepe22@hotmail.com>
2024-02-07 15:10:18 -0600

    fetched run data and passed to runTime

--- app/dashboard/dynamis/layout.tsx
+++ app/dashboard/dynamis/layout.tsx
@@ -1,9 +1,13 @@
+import { getRuns } from "@/app/lib/data";
 import Attendance from "@/app/ui/attendance";
+import RunTime from "@/app/ui/runTime";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+	const runsData = await getRuns()
 	return (
 		<main className="w-full h-full flex gap-[2px]">
-			<div className="w-full md:w-[300px] bg-stone-700 rounded-md rounded-tr-none rounded-br-none p-3 h-full">
+			<div className="w-full md:w-[600px] bg-stone-700 rounded-md rounded-tr-none rounded-br-none p-3 h-full">
+				<RunTime runs={runsData.runs} />
 				<Attendance />
 			</div>
 			<div className="w-full md:w-full bg-stone-700 rounded-md rounded-tl-none rounded-bl-none p-3">

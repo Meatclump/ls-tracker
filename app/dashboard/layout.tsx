35b9d4d4c2046fb3a8ec6b61b4965dfbe1e61d13
Henrik Persson <hepe22@hotmail.com>
2024-02-07 15:09:23 -0600

    shrunk sidebar width somewhat

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main className="flex h-screen flex-col md:flex-row md:overflow-hidden p-1 bg-gray-300 dark:bg-stone-950 gap-1">
-			<div className="w-full flex-none md:w-64 bg-gray-100 dark:bg-stone-600 rounded-md">
+			<div className="w-full flex-none md:w-56 bg-gray-100 dark:bg-stone-600 rounded-md">
 				<SideNav />
 			</div>
 			<div className="flex-grow h-full md:overflow-y-auto">
